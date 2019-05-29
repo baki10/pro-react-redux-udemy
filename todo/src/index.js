1e22db71e6e8b7cbc87b0ccd3111f68387293da0
Ilmir Bakirov <bakirov@xrm.ru>
2019-05-29 23:45:38 +0500

    create first lightweight react element

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,1 +1,15 @@
-console.log("hello world");
+import React from "react";
+import ReactDOM from "react-dom";
+
+const el = (
+    <div>
+        <h1>My Todo List</h1>
+        <input placeholder='Search' />
+        <ul>
+            <li>Learn ES6</li>
+            <li>Learn React</li>
+        </ul>
+    </div>
+);
+
+ReactDOM.render(el, document.getElementById('root'))
