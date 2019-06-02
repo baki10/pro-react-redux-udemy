2bf47aa0350ea65eff53204aa093b2616ea0ec28
Ilmir Bakirov <bakirov@xrm.ru>
2019-06-02 21:23:52 +0500

    refactoring: change function to class component

--- todo/src/components/todo-list-item/todo-list-item.js
+++ todo/src/components/todo-list-item/todo-list-item.js
@@ -1,27 +1,30 @@
 import React from "react";
 import './todo-list-item.css';
 
-const TodoListItem = ({ label, important = false }) => {
-    const itemStyle = {
-        color: important ? 'steelblue' : 'black',
-        fontWeight: important ? 'bold' : 'normal'
-    };
-    return (
-        <span className='todo-list-item'>
-            <span style={itemStyle} className='todo-list-item-label'>
-                {label}
+export default class TodoListItem extends React.Component {
+
+    render() {
+        const { label, important = false } = this.props;
+
+        const itemStyle = {
+            color: important ? 'steelblue' : 'black',
+            fontWeight: important ? 'bold' : 'normal'
+        };
+        return (
+            <span className='todo-list-item'>
+                <span style={itemStyle} className='todo-list-item-label'>
+                    {label}
+                </span>
+
+                <button type='button'
+                    className='btn btn-outline-success btn-sm float-right'>
+                    <i className='fa fa-exclamation'></i>
+                </button>
+                <button type='button'
+                    className='btn btn-outline-danger btn-sm float-right'>
+                    <i className='fa fa-trash-o'></i>
+                </button>
             </span>
-
-            <button type='button'
-                className='btn btn-outline-success btn-sm float-right'>
-                <i className='fa fa-exclamation'></i>
-            </button>
-            <button type='button'
-                className='btn btn-outline-danger btn-sm float-right'>
-                <i className='fa fa-trash-o'></i>
-            </button>
-        </span>
-    );
-};
-
-export default TodoListItem;
+        );
+    }
+}
